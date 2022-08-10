820b2bd8a68c3940adbbfd73e0a738cd275e2ea4
jz3453 <97906628+jz3453@users.noreply.github.com>
2022-08-10 16:37:08 -0400

    Update index.js

--- lambda/index.js
+++ lambda/index.js
@@ -1,8 +1,4 @@
-/* *
- * This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
- * Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
- * session persistence, api calls, and more.
- * */
+
 const Alexa = require('ask-sdk-core');
 const axios = require('axios');
 const logic = require('./logic');
